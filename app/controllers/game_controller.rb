66a0a9521727898aaa0716814041638aff596eb8
a-ruizdel <a-rdl@wireless-s1-no-150-19-119.uchicago.edu>
2015-07-27 18:08:36 -0500

    Week 5 Homework after adding m save

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -27,9 +27,32 @@ class GameController < ApplicationController
       @outcome = "won"
     end
 
+    m= Move.new
+    m.user_move = @user_move
+    m.computer_move = @computer_move
+
+    if @outcome == "won"
+      m.user_wins = 1
+      m.computer_wins = 0
+    elsif @outcome == "lost"
+      m.user_wins = 0
+      m.computer_wins = 1
+    elsif @outcome == "tied"
+      m.tie = 1
+    else
+      m.user_wins = 0
+      m.computer_wins = 0
+      m.tie = 0
+    end
+
+    m.save
+
+    @all_moves = Move.all
+
+    @count_total_moves = Move.count
 
 
-    render ("games/rock.html.erb")
-  end
-
+    render ("games/move.html.erb")
+#end
+end
 end
